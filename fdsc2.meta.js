6f7ff4dd39df8d3345568c7a8617b25a51dd63bf
prateekmaddy <prateekshrivastava00@gmail.com>
2019-11-29 10:22:52 -0700

    Update fdsc2.meta.js

--- fdsc2.meta.js
+++ fdsc2.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Destination Caps Highlighter
-// @version  1.4
+// @version  1.5
 // @grant    none
 // @include  *
 // @updateURL   https://raw.githubusercontent.com/prateekmaddy/user_script/master/fdsc2.meta.js
@@ -34,44 +34,30 @@
         p.replaceChild(frag, textNode);
     }
 }
-highlightWord('ATL6-->DBM2-CYC1')
 highlightWord('AZA5-->DPX6-CYC2')
 highlightWord('AZA5-->DPX6-CYC2')
 highlightWord('AZA5-->DPX6-CYC2')
 highlightWord('AZA5-->DPX6-CYC2')
-highlightWord('BOS5-->DBO1-CYC1')
-highlightWord('BOS5-->DBO2-CYC1')
-highlightWord('BOS5-->DBO6-CYC1')
-highlightWord('BOS5-->DRI1-CYC1')
-highlightWord('BOS7-->DRI1-A')
-highlightWord('CAE1-->DBM2-B')
-highlightWord('CHA1-->DBM2-B')
-highlightWord('DAL9-->DDA1-CYC1')
+highlightWord('DAL9-->DDA1-CYC2')
 highlightWord('DEN5-->DDE5-CYC2')
 highlightWord('DEN5-->DDE5-CYC2')
 highlightWord('DEN5-->DDE5-CYC2')
 highlightWord('DEN5-->DDE5-CYC2')
-highlightWord('DFW8-->DDA1-CYC1')
-highlightWord('DTW5-->DDT1-CYC1')
-highlightWord('EWR8-->DEW1')
-highlightWord('FAT1-->DSF5-B')
-highlightWord('JFK8-->DRI1-A')
-highlightWord('LAS5-->DLV1-CYC1')
+highlightWord('EWR4-->DRO1-A')
+highlightWord('HOU1-->DHO1-CYC2')
+highlightWord('HOU1-->DHO1-CYC2')
+highlightWord('HOU1-->DHO1-CYC2')
+highlightWord('HOU1-->DHO1-CYC2')
+highlightWord('JFK8-->DRO1-A')
 highlightWord('MCI5-->DMC1-CYC2')
 highlightWord('MCI5-->DMC1-CYC2')
 highlightWord('MCI5-->DMC1-CYC2')
 highlightWord('MCI5-->DMC1-CYC2')
 highlightWord('MCI5-->DMC2-CYC2')
-highlightWord('MCO1-->DMO3-B')
-highlightWord('MCO5-->DMO3-CYC1')
-highlightWord('MCO9-->DMO3-CYC1')
-highlightWord('MDW5-->DCH7-CYC1')
-highlightWord('MSP9-->DMP1-CYC1')
+highlightWord('MKE5-->DML2-CYC2')
+highlightWord('MKE5-->DML2-CYC2')
+highlightWord('MKE5-->DML2-CYC2')
+highlightWord('MKE5-->DML2-CYC2')
 highlightWord('MSP9-->DMS1-CYC2')
-highlightWord('OAK4-->DSF5-B')
-highlightWord('ONT2-->DLV1-A')
-highlightWord('RNO4-->DSF5-B')
-highlightWord('SMF1-->DSF5-B')
-highlightWord('STL5-->DLI1-CYC1')
-highlightWord('TPA2-->DMO3-B');
-
+highlightWord('PDX5-->DPD1-CYC2')
+highlightWord('TPA1-->DBM2-A');
